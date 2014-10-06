c664c315687e19ba73f74d5af435d92ed84b2ed2
skymanzanares@hotmail.com <skymanzanares@hotmail.com>
2014-10-05 19:46:55 -0600

    Added the onLoad, onEnable, onDisable methods to plugin. Created plugin.yml

--- src/com/SkyIsland/CTF/CTFPlugin.java
+++ src/com/SkyIsland/CTF/CTFPlugin.java
@@ -4,7 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CTFPlugin extends JavaPlugin {
 	
+	public void onLoad() {
+		
+	}
 	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
 	
 	
 }
